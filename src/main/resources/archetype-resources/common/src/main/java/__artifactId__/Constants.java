06f3e4fb17a40ac977a2613df3b8fd08f6610919
misolab <do.ockhyun@gmail.com>
2020-11-19 10:44:24 +0900

    add common module

---
+++ src/main/resources/archetype-resources/common/src/main/java/__artifactId__/Constants.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${artifactId};
+
+import com.misolab.core.code.Constant;
+
+public interface Constants extends Constant {
+
+    String PROJECT = "misolab-java-multi";
+
+}
