88b2adfeab6f7ca1601d8209ba62e4f3ff3c5259
misolab <do.ockhyun@gmail.com>
2021-01-24 10:25:47 +0900

    add WebSecurityConfig, SiteSecurityConfig

---
+++ src/main/resources/archetype-resources/site/src/main/java/__artifactId__/config/SiteSecurityConfig.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${artifactId}.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import ${package}.web.config.WebSecurityConfig;
+
+@EnableWebSecurity
+public class SiteSecurityConfig extends WebSecurityConfig {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
---
+++ src/main/resources/archetype-resources/web/src/main/java/__artifactId__/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${artifactId}.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
